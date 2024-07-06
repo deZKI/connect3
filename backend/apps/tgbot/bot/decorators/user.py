1c98287c96df5b45a6d8bdd39c682d9c543effef
kiril <kirill.301522@gmail.com>
2024-07-06 20:16:10 +1000

    register

--- backend/apps/tgbot/bot/decorators/user.py
+++ backend/apps/tgbot/bot/decorators/user.py
@@ -3,6 +3,7 @@ from typing import Any, Callable
 from aiogram import types
 
 from apps.users.models import UserExtended
+from aiogram.fsm.context import FSMContext
 
 
 def user_is_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
@@ -35,11 +36,11 @@ def user_is_not_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
     return wrapper
 
 
-def user_is_not_registered(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    async def wrapper(event: types.Message) -> None:
+def user_is_not_registered(handler: Callable[[Any, Any], Any]) -> Callable[[Any], Any]:
+    async def wrapper(event: types.Message, state: FSMContext) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if not user.is_registered:
-            await handler(event)
+            await handler(event, state)
 
     return wrapper
