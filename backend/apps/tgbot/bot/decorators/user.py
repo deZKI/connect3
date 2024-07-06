a46282d668f2baf0c9c82fd44859fbb0398033a3
kiril <kirill.301522@gmail.com>
2024-07-06 23:27:04 +1000

    args

--- backend/apps/tgbot/bot/decorators/user.py
+++ backend/apps/tgbot/bot/decorators/user.py
@@ -3,11 +3,10 @@ from typing import Any, Callable
 from aiogram import types
 
 from apps.users.models import UserExtended
-from aiogram.fsm.context import FSMContext
 
 
 def user_is_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    async def wrapper(event: types.Message) -> None:
+    async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if user.is_payed:
@@ -17,7 +16,7 @@ def user_is_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
 
 
 def user_is_registered(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    async def wrapper(event: types.Message) -> None:
+    async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if user.is_registered:
@@ -27,7 +26,7 @@ def user_is_registered(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
 
 
 def user_is_not_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
-    async def wrapper(event: types.Message) -> None:
+    async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if not user.is_payed:
@@ -37,10 +36,10 @@ def user_is_not_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
 
 
 def user_is_not_registered(handler: Callable[[Any, Any], Any]) -> Callable[[Any], Any]:
-    async def wrapper(event: types.Message, state: FSMContext) -> None:
+    async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if not user.is_registered:
-            await handler(event, state)
+            await handler(event, *args)
 
     return wrapper
