b005f5545939805dbb74e3d9dafbb2a417e430ce
kiril <kirill.301522@gmail.com>
2024-07-06 23:30:40 +1000

    args

--- backend/apps/tgbot/bot/decorators/user.py
+++ backend/apps/tgbot/bot/decorators/user.py
@@ -5,32 +5,32 @@ from aiogram import types
 from apps.users.models import UserExtended
 
 
-def user_is_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
+def user_is_payed(handler: Callable[[Any, Any], Any]) -> Callable[[Any], Any]:
     async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if user.is_payed:
-            await handler(event)
+            await handler(event, *args)
 
     return wrapper
 
 
-def user_is_registered(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
+def user_is_registered(handler: Callable[[Any, Any], Any]) -> Callable[[Any], Any]:
     async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if user.is_registered:
-            await handler(event)
+            await handler(event,  *args)
 
     return wrapper
 
 
-def user_is_not_payed(handler: Callable[[Any], Any]) -> Callable[[Any], Any]:
+def user_is_not_payed(handler: Callable[[Any, Any], Any]) -> Callable[[Any], Any]:
     async def wrapper(event: types.Message, *args) -> None:
         tg_user = event.from_user
         user = await UserExtended.objects.aget(tg_chat_id=tg_user.id, username=tg_user.username)
         if not user.is_payed:
-            await handler(event)
+            await handler(event, *args)
 
     return wrapper
 
