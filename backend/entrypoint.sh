4d27f37c54064830400583b38fc18b1fe5514e95
root <root@cv3989553.novalocal>
2024-07-29 17:22:32 +0300

    commit

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,13 +11,15 @@ echo "PostgreSQL started"
 # Выполнение миграций
 python manage.py migrate users
 python manage.py migrate --noinput
+echo "Migrations"
 
 # Запуск start_bott
 python manage.py start_bot &
+echo "start_bott"
 
 # Сбор статических файлов
 python manage.py collectstatic --noinput --clear
-
+echo "collectstatic"
 # Запуск Gunicorn или любого другого WSGI сервера, который вы используете
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers=4
 # Start server
